bbc4e7e953ba59f0a5cd44587107910ea3a1ca37
Nihiler <3529201995@qq.com>
2024-06-27 16:30:24 +0800

    对用户密码进行哈希算法转化以后再存储在数据库中，提供安全性保障。

--- src/main/java/ymaker/javaweb/studyhabitsplan/server/service/Userservice.java
+++ src/main/java/ymaker/javaweb/studyhabitsplan/server/service/Userservice.java
@@ -2,6 +2,7 @@ package ymaker.javaweb.studyhabitsplan.server.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 import ymaker.javaweb.studyhabitsplan.common.Context.BaseContext;
 import ymaker.javaweb.studyhabitsplan.pojo.User;
 import ymaker.javaweb.studyhabitsplan.server.mapper.UserMapper;
@@ -12,10 +13,13 @@ public class Userservice implements UserServiceInter{
     public UserMapper userMapper;
     @Override
     public void register(String username, String password) {
-                userMapper.insert(username, password);
+        //对密码进行md5哈希算法转化以后再存储到数据库里面
+        String s = DigestUtils.md5DigestAsHex(password.getBytes());
+        userMapper.insert(username,s);
     }
 
     public User login(User user) {
+        user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes()));
         User q = userMapper.login(user);
         if(q !=null){
             BaseContext.setCurrentUsername(q.getUsername());
